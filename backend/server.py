d9f53870204073989d798cb53fe91c134c1610f9
fares-not-present <fareseddine2025@gmail.com>
2025-04-02 18:50:41 +0200

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -12,7 +12,7 @@ app = FastAPI()
 # ✅ CORS Middleware (Allow Frontend)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://frontend-kbcefz2f7-fares-projects-d76a0c1b.vercel.app"],
+    allow_origins=["https://frontend-9r3gcxn9y-fares-projects-d76a0c1b.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
